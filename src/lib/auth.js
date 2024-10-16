b2d82926bf6b617e4e69382976f66f9f78ea70f4
Rishabh Raj Pathak <rishabhrajpathak06@gmail.com>
2024-10-16 16:58:23 +0530

    hh

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,7 +1,7 @@
 import { db } from "@/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import { AuthOptions, DefaultSession, getServerSession } from "next-auth";
-import { Adapter } from "next-auth/adapters";
+import { getServerSession } from "next-auth";
+
 import GoogleProvider from "next-auth/providers/google";
 
 export const authConfig = {
